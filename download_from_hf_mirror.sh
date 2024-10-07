0aeb40f8917bb4152aaf6c16102964d8776fab44
ShacklesLay <shackleslay@gmail.com>
2024-10-07 16:44:57 +0800

    Add

--- download_from_hf_mirror.sh
+++ download_from_hf_mirror.sh
@@ -1,5 +1,5 @@
 export HF_ENDPOINT=https://hf-mirror.com
 # 模型
-huggingface-cli download --resume-download gpt2 --local-dir gpt2 --local-dir-use-symlinks False
+huggingface-cli download --resume-download openai/clip-vit-large-patch14-336 --local-dir /remote-home1/share/models/vision_encoder/clip-vit-large-patch14-336-openai --local-dir-use-symlinks False
 # 数据集
-# huggingface-cli download --repo-type dataset --resume-download wikitext --local-dir wikitext --local-dir-use-symlinks False
+# huggingface-cli download --repo-type dataset --resume-download liuhaotian/LLaVA-Pretrain --local-dir wikitext --local-dir-use-symlinks False
