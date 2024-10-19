c9074f3c0617f632238a893b3257cbbcc4c14b97
ShacklesLay <shackleslay@gmail.com>
2024-10-19 12:49:43 +0800

    fix bugs

--- download_from_hf_mirror.sh
+++ download_from_hf_mirror.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=a800
+
+#SBATCH --job-name=download
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=2G
+
+#SBATCH --chdir=/remote-home1/cktan
+#SBATCH --output=./log.log
+
 export HF_ENDPOINT=https://hf-mirror.com
 # 模型
-huggingface-cli download --resume-download openai/clip-vit-large-patch14-336 --local-dir /remote-home1/share/models/vision_encoder/clip-vit-large-patch14-336-openai --local-dir-use-symlinks False
+srun huggingface-cli download --resume-download BAAI/Emu3-Chat --local-dir /remote-home1/share/models/Emu3-Chat --local-dir-use-symlinks False
 # 数据集
 # huggingface-cli download --repo-type dataset --resume-download liuhaotian/LLaVA-Pretrain --local-dir wikitext --local-dir-use-symlinks False
+
+sync && echo "success"
